7d1b1cb9490d13a14421372d16ad2adad9160cb0
vimm0 <vimmrana0@gmail.com>
2018-12-15 20:31:38 +0545

    setup axios

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
     <div id="app">
-        <navbar></navbar>
+        <!--<navbar></navbar>-->
         <landing-content></landing-content>
-        <page-footer></page-footer>
+        <!--<page-footer></page-footer>-->
     </div>
 </template>
 
@@ -11,12 +11,25 @@
     import LandingContent from './components/LandingContent.vue'
     import Footer from './components/Footer.vue'
 
+    import axios from 'axios'
+
     export default {
         name: 'app',
         components: {
             Navbar,
             LandingContent,
             'page-footer': Footer
+        },
+        created() {
+            axios.defaults.baseURL = 'http://client1.nepexgroup.com:8000';
+            axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN'
+            axios.defaults.xsrfCookieName = 'csrftoken'
+            axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+            // PER CLIENT CONFIG
+            axios.defaults.auth = {
+                username: 'client1',
+                password: 'admin'
+            }
         }
     }
 </script>
@@ -60,7 +73,7 @@
     }
 
     /*svg.social-media path {*/
-        /*fill: #fff;*/
+    /*fill: #fff;*/
     /*}*/
 
     .card-content {
--- src/components/LandingContent.vue
+++ src/components/LandingContent.vue
@@ -1,201 +1,43 @@
 <template>
-    <div class="landing-content">
-        <!--<div class="image-container" style="margin-bottom:17.8rem">-->
-        <!--<img alt="Vue logo" src="../assets/school-banner.jpg">-->
-        <div class="container">
-            <div class="columns" style="margin-bottom:1rem">
-                <div class="column is-three-quarters">
-                    <p class="is-size-1 has-text-weight-semibold">Better Education For Better World.</p>
-                    <p style="font-size:.85rem;">Morbi tristique massa nulla, nec hendrerit quam
-                        iaculis ac. Praesent a lorem at arcu porttitor pharetra. Suspendisse ante nibh, venenatis velssa
-                        laoreet sit amet, iaculis at diam. </p>
-                    <div class="columns is-pulled-right">
-                        <div class="column">
-                            <a class="button is-outlined is-uppercase ">View Courses</a>
-                        </div>
-                        <div class="column">
-                            <a class="button is-outlined is-uppercase">Read More
-                                &nbsp;<font-awesome-icon icon="long-arrow-alt-right"></font-awesome-icon>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="column">
-                    <!--:href="`https://www.facebook.com/sharer/sharer.php?u=${url}&amp;src=sdkpreparse`"-->
-                    <div class="columns is-pulled-right">
-                        <div class="column">
-                            <a class=""
-                               href="#"
-                               target="_blank">
-                                <svg class="social-media" xmlns="http://www.w3.org/2000/svg" width="24" height="24">
-                                    <path d="M12 0a12 12 0 1 0 0 24 12 12 0 0 0 0-24zm3 8h-1.35c-.54 0-.65.22-.65.78V10h2l-.2 2H13v7h-3v-7H8v-2h2V7.7c0-1.78.93-2.7 3.03-2.7H15v3z"></path>
-                                </svg>
-                            </a>
-                        </div>
-                        <div class="column">
-                            <a class="" :href="`https://twitter.com/intent/tweet?url=${url};text=konichiwa`">
-                                <svg class="social-media" xmlns="http://www.w3.org/2000/svg" width="24" height="24">
-                                    <path d="M12 0a12 12 0 1 0 0 24 12 12 0 0 0 0-24zm6.07 9.64A8.12 8.12 0 0 1 9.9 18.2a8.13 8.13 0 0 1-4.4-1.3 5.78 5.78 0 0 0 4.25-1.18 2.88 2.88 0 0 1-2.68-2c.45.09.9.06 1.3-.05a2.88 2.88 0 0 1-2.3-2.85c.38.22.82.35 1.3.36a2.88 2.88 0 0 1-.9-3.83 8.15 8.15 0 0 0 5.92 3 2.88 2.88 0 0 1 4.9-2.62 5.73 5.73 0 0 0 1.82-.7 2.88 2.88 0 0 1-1.26 1.59 5.73 5.73 0 0 0 1.65-.45 5.77 5.77 0 0 1-1.43 1.49z"></path>
-                                </svg>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="columns" style="margin-bottom:1rem">
-                <div class="column">
-                    <p class="is-size-1 has-text-weight-semibold">Welcome To Brand Name.</p>
-                    <p style="font-size:.85rem;color:#666666;">Aorem ipsum dolor sit amet, consectetur adipisicing elit,
-                        sed do
-                        eiusmod does tempor incididunt ut labore et.dolore magna aliqua. Ut enim ad minim veniam quis
-                        nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat quis nostrud
-                        exercitation ullamco. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                        dolore eu fugiat nulla pariatu </p>
-                    <a class="button is-uppercase mt-1 read-more is-pulled-right">Read More
-                        &nbsp;<font-awesome-icon icon="long-arrow-alt-right"></font-awesome-icon>
-                    </a>
-                </div>
-                <div class="column"></div>
-            </div>
-
-        </div>
-        <div class="columns" style="background:#fafafa;">
-            <div class="column has-text-centered">
-                <p class="is-size-2">Popular Courses</p>
-            </div>
-        </div>
-        <!--<div class="column is-half">-->
-
-        <!--</div>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--<div class="container">-->
-        <!--<div class="columns landing-card-columns">-->
-        <!--<div class="column is-one-third" data-aos="fade-up"-->
-        <!--data-aos-once="false">-->
-        <!--<div class="card">-->
-        <!--<div class="card-content card-content-1 has-text-white-ter has-text-centered">-->
-        <!--<img src="../assets/slide/slide-bottom-01.png" alt="professional-teachers"-->
-        <!--class="btm-item-icon"-->
-        <!--style="">-->
-        <!--<p class="is-size-4 has-text-weight-semibold">Professional Teachers</p>-->
-        <!--<div class="content is-size-7">-->
-        <!--Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis-->
-        <!--mauris.-->
-        <!--<br>-->
-        <!--<time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>-->
-        <!--<br>-->
-        <!--<a class="mt-1 is-size-5 is-uppercase has-text-white-ter">Read More</a>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--<div class="column is-one-third" data-aos="fade-up">-->
-        <!--<div class="card">-->
-        <!--<div class="card-content card-content-2 has-text-white-ter has-text-centered">-->
-        <!--<img src="../assets/slide/slide-bottom-02.png" alt="learn-anywhere-online"-->
-        <!--class="btm-item-icon">-->
-        <!--<p class="is-size-4 has-text-weight-semibold">Learn Anywhere Online</p>-->
-        <!--<div class="content is-size-7">-->
-        <!--Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis-->
-        <!--mauris.-->
-        <!--<br>-->
-        <!--<time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>-->
-        <!--<br>-->
-        <!--<a class="mt-1 is-size-5 is-uppercase has-text-white-ter">Read More</a>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--<div class="column is-one-third" data-aos="fade-up">-->
-        <!--<div class="card">-->
-        <!--<div class="card-content card-content-3 has-text-white-ter has-text-centered">-->
-        <!--<img src="../assets/slide/slide-bottom-03.png" alt="graducation-certificate"-->
-        <!--class="btm-item-icon">-->
-        <!--<p class="is-size-4 has-text-weight-semibold">Graduation Certificate</p>-->
-        <!--<div class="content is-size-7">-->
-        <!--Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis-->
-        <!--mauris.-->
-        <!--<br>-->
-        <!--<time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>-->
-        <!--<br>-->
-        <!--<a class="mt-1 is-size-5 is-uppercase has-text-white-ter">Read More</a>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--</div>-->
+    <div>
+        <!--<form>-->
+        <!--<input class="input" type="text" placeholder="Your Name" v-model="name">-->
+        <!--<input class="input" type="email" placeholder="Your Email" v-model="email">-->
+        <!--<input class="input" type="text" placeholder="Subject" v-model="subject">-->
+        <!--<textarea class="textarea" placeholder="e.g. Hello world" v-model="message"></textarea>-->
+        <!--<a class="button" @click="sendMessage">Send Message</a>-->
+        <!--</form>-->
     </div>
-    <!--<div class="container">-->
-    <!--<div class="columns">-->
-    <!--<div class="column">-->
-
-    <!--<div class="video-block">-->
-    <!--<div id="thumbnail_container"><img src="../assets/about-video.png" id="thumbnail"-->
-    <!--class="img-responsive" alt=""></div>-->
-    <!--&lt;!&ndash;<a href="https://www.youtube.com/embed/_P8ZG7fWp7A" class="start-video video"><img&ndash;&gt;-->
-    <!--&lt;!&ndash;src="../assets/play-btn.png" alt=""></a>&ndash;&gt;-->
-    <!--</div>-->
-    <!--&lt;!&ndash;<iframe width="450" height="300"&ndash;&gt;-->
-    <!--&lt;!&ndash;src="https://www.youtube.com/embed/_P8ZG7fWp7A"&ndash;&gt;-->
-    <!--&lt;!&ndash;frameborder="0"&ndash;&gt;-->
-    <!--&lt;!&ndash;allowfullscreen></iframe>&ndash;&gt;-->
-    <!--</div>-->
-    <!--<div class="column">-->
-    <!--<p class="is-size-3">Sed quis justo.</p>-->
-    <!--<p class="mt-1" style="font-size:.8rem;text-align:justify">Ut bibendum, sapien ac tristique-->
-    <!--venenatis, tellus sem-->
-    <!--tempor arcu, faucibus commodo ante tellus sed justo. In turpis velit, mattis nec vehicula at,-->
-    <!--gravida eget erat.-->
-    <!--Morbi feugiat vel lorem eget malesuada. Curabitur eget vulputate risus. Phasellus faucibus-->
-    <!--sagittis-->
-    <!--efficitur. Pellentesque faucibus libero in nibh elementum elementum. Nullam ultricies tellus-->
-    <!--eget purus-->
-    <!--feugiat, vitae posuere velit accumsan. Nunc placerat venenatis ullamcorper. Ut imperdiet-->
-    <!--imperdiet velit. In-->
-    <!--odio sem, placerat non mattis at, aliquet sed velit. Maecenas bibendum neque vitae lorem-->
-    <!--laoreet, sed-->
-    <!--ornare diam suscipit. Aliquam erat volutpat. Cras malesuada nunc et tincidunt accumsan. Quisque-->
-    <!--consequat-->
-    <!--sit amet odio sit amet feugiat. Cras posuere odio quam, venenatis euismod eros laoreet accumsan.-->
-    <!--Phasellus sed-->
-    <!--pellentesque turpis.</p>-->
-    <!--<a class="button is-uppercase mt-1 read-more">Read More-->
-    <!--&nbsp;<font-awesome-icon icon="long-arrow-alt-right"></font-awesome-icon>-->
-    <!--</a>-->
-    <!--</div>-->
-    <!--</div>-->
-    <!--<p class="is-size-3 has-text-centered">Sed quis ligula efficitur, varius felis.-->
-    <!--Uttellus sem tempor arcu, faucibus commodo ante tellus sed justo.</p>-->
-    <!--<p class="is-size-6 has-text-centered mt-1" data-aos="fade-up"-->
-    <!--data-aos-offset="200"-->
-    <!--data-aos-delay="50"-->
-    <!--data-aos-duration="1000"-->
-    <!--data-aos-easing="ease-in-out"-->
-    <!--data-aos-mirror="true"-->
-    <!--data-aos-once="false"-->
-    <!--data-aos-anchor-placement="top-center">Ut bibendum, sapien ac tristique venenatis, tellus sem tempor-->
-    <!--arcu,-->
-    <!--faucibus commodo ante tellus sed justo. In turpis velit, mattis nec vehicula at, gravida eget erat.-->
-    <!--Morbi-->
-    <!--feugiat vel lorem eget malesuada. Curabitur eget vulputate risus. Phasellus faucibus sagittis efficitur.-->
-    <!--Pellentesque faucibus libero in nibh elementum elementum. Nullam ultricies tellus eget purus feugiat,-->
-    <!--vitae-->
-    <!--posuere velit accumsan. Nunc placerat venenatis ullamcorper. Ut imperdiet imperdiet velit. In odio sem,-->
-    <!--placerat non mattis at, aliquet sed velit. Maecenas bibendum neque vitae lorem laoreet, sed ornare diam-->
-    <!--suscipit. Aliquam erat volutpat. Cras malesuada nunc et tincidunt accumsan. Quisque consequat sit amet-->
-    <!--odio-->
-    <!--sit amet feugiat. Cras posuere odio quam, venenatis euismod eros laoreet accumsan. Phasellus sed-->
-    <!--pellentesque turpis.</p>-->
-    <!--</div>-->
 </template>
 <script>
+    import axios from 'axios'
+
     export default {
         name: 'landing-content',
         data() {
             return {
-                url: 'https://vimm0.github.io/'
+                url: 'https://vimm0.github.io/',
+                name: '',
+                email: '',
+                subject: '',
+                message: '',
+            }
+        },
+        methods: {
+            sendMessage() {
+                // console.log(this.name + ' ' + this.email + ' ' + this.subject + ' ' + this.message)
+                axios.post('/message/', {
+                    name: 'FASDFASD',
+                    email: 'vimmrana0@gmail.com',
+                    subject: 'FlintsFASDFASFtone',
+                    message: 'FlintAFSDFASstone'
+                })
+                    .then(function (response) {
+                        console.log(response.data);
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
             }
         }
     }
